6d532722193bce305fb7718f802b62a2d85947fd
Brooklyn Photos <guoneng.list@gmail.com>
2019-12-17 13:02:10 +0100

    Added documntation

--- correlation/popularity/twitter.py
+++ correlation/popularity/twitter.py
@@ -4,6 +4,9 @@ Interface to the twitter API to retrieve raw data from twitter
 
 class TwitterRetriever:
   def __init__(self, tag):
+    """
+    The twitter hash that we are following
+    """
     self.tag = tag
 
   def retrieve(self):
